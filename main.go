39fff2c1da1103fe81b8bb7e26af4372111d7fe6
Vince Hines <vinjh3@gmail.com>
2025-06-02 15:28:41 -0400

    sets up the basic server

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// Create a new Gin router 
+	r := gin.Default()
+
+	// healthcheck endpoint
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"status": "ok"})
+	})
+
+	//TODO: Add more endpoints here
+
+	log.Println("Starting server on :8080")
+	if err := r.Run(":8080"); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
